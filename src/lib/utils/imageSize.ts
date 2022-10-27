b5ba1911c242e879a9d0da167a2f93b416c33493
Robotechnic <robotechnic.developeur@gmail.com>
2022-10-28 00:49:36 +0200

    add tile organisation + resize images when they are first processed

---
+++ src/lib/utils/imageSize.ts
@@ -0,0 +1,22 @@
+export const imageResize = (
+	image: HTMLImageElement,
+	width: number,
+	heigth: number
+) => {
+	const canvas = document.createElement("canvas")
+	const ctx = canvas.getContext("2d")
+	if (!ctx) throw new Error("Canvas context is not defined")
+	canvas.width = width
+	canvas.height = heigth
+	ctx.imageSmoothingEnabled = true
+	ctx.imageSmoothingQuality = "high"
+	ctx.drawImage(image, 0, 0, width, heigth)
+	const result: HTMLImageElement = new Image()
+	result.src = canvas.toDataURL()
+	result.alt = image.alt
+	result.title = image.title
+	console.log(image)
+	console.log(result)
+
+	return result
+}
